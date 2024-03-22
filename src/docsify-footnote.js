f911017fec9a9812b674e797c58f92c553fff108
Robert <duhang.111@qq.com>
2024-03-22 16:42:39 +0800

    fix: replace strings formatted as footnote within code blocks (#1)

--- src/docsify-footnote.js
+++ src/docsify-footnote.js
@@ -1,16 +1,25 @@
 (function () {
     var footnotePlugin = function (hook, vm) {
-        hook.beforeEach(function(html) {
+        hook.beforeEach(function(markdown) {
+            console.log(markdown);
             const footnoteRegex = /(\[\^.+\]:\s.*(\n|$))|(\^\[.*?\])/gm;
 
-            if (footnoteRegex.test(html)) {
-                const matchList = html.match(footnoteRegex);
+            // To temporarily bypass footnotes within code blocks, replace all code blocks with placeholders first.
+            const codeBlocks = markdown.match(/```.*?```/gsm);
+            if (codeBlocks) {
+                codeBlocks.forEach(function(e, i) {
+                    markdown = markdown.replace(e, '```'+i+'```');
+                })
+            }
+
+            if (footnoteRegex.test(markdown)) {
+                const matchList = markdown.match(footnoteRegex);
 
                 const footnoteList = [];
                 matchList.forEach(function(e, i) {
                     // inline-style footnote
                     if (/\^\[.*?\]/.test(e)) {
-                        html = html.replace(
+                        markdown = markdown.replace(
                             e, 
                             `<sup class="footnote-symbol" id="ft-${i}">[\[${i+1}\]](#ftref-${i})</sup>`
                         );
@@ -21,7 +30,7 @@
                     }else { // reference-style footnote
                         const noteMap = e.split(': ');
 
-                        html = html.replace(
+                        markdown = markdown.replace(
                             noteMap[0], 
                             `<sup class="footnote-symbol" id="ft-${i}">[\[${i+1}\]](#ftref-${i})</sup>`
                         ).replace(e, '');
@@ -32,10 +41,17 @@
                     }
                 })
 
-                html += `\n---\n ${footnoteList.join('')}`;
+                markdown += `\n---\n ${footnoteList.join('')}`;
             }
 
-            return html;
+            // restore code blocks
+            if (codeBlocks) {
+                codeBlocks.forEach(function(e, i) {
+                    markdown = markdown.replace('```'+i+'```', e);
+                });
+            }
+
+            return markdown;
         });
     };
 
